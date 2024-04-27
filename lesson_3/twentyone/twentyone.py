49f52c603343285eeaa5800b45a8b1017088fab9
Caiqian Cropper <caiqian.wu@gmail.com>
2024-04-26 21:13:00 -0500

    add deal_cards

--- lesson_3/twentyone/twentyone.py
+++ lesson_3/twentyone/twentyone.py
@@ -1,6 +1,7 @@
 import random
+from utils import prompt
 
-deck = [
+COMPLETE_DECK = [
     ["Hearts", "A"], ["Diamonds", "A"], ["Clubs", "A"], ["Spades", "A"],
     ["Hearts", "2"], ["Diamonds", "2"], ["Clubs", "2"], ["Spades", "2"],
     ["Hearts", "3"], ["Diamonds", "3"], ["Clubs", "3"], ["Spades", "3"],
@@ -16,15 +17,39 @@ deck = [
     ["Hearts", "K"], ["Diamonds", "K"], ["Clubs", "K"], ["Spades", "K"]
 ]
 
-def shuffle(deck):
-    random.shuffle(deck)
+player_hand = []
+computer_hand = []
+
+def shuffle(cards):
+    random.shuffle(cards)
+
+def initialize_deck():
+    playing_deck = COMPLETE_DECK
+    shuffle(playing_deck)
+    return playing_deck
+
+def deal_cards(deck, player):
+    # need to grab 2 card from deck
+    if player == 'human':
+        player_hand.append(deck.pop())
+        player_hand.append(deck.pop())
+    elif player == 'computer':
+        computer_hand.append(deck.pop())
+        computer_hand.append(deck.pop())
+    else:
+        pass
 
 # 1. Initialize deck
 
-shuffle(deck)
+playing_deck = initialize_deck()
 
 # 2. Deal cards to player and dealer
 
+deal_cards(playing_deck, 'human')
+deal_cards(playing_deck, 'computer')
+
+print(player_hand)
+print(computer_hand)
 
 # 3. Player turn: hit or stay
 #    - repeat until bust or stay
