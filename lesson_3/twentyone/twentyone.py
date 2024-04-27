0cf10b982f8aab2fde4bcb69c5b89d2fd7c8de3e
Caiqian Cropper <caiqian.wu@gmail.com>
2024-04-27 10:10:43 -0500

    working version of game

--- lesson_3/twentyone/twentyone.py
+++ lesson_3/twentyone/twentyone.py
@@ -1,3 +1,4 @@
+import os
 import random
 from utils import prompt, join_or
 
@@ -20,10 +21,15 @@ COMPLETE_DECK = [
 player_hand = []
 computer_hand = []
 
+def prompt_with_separator(msg):
+    print(f"{'~' * 60}")
+    prompt(msg)
+
 def shuffle(cards):
     random.shuffle(cards)
 
 def initialize_deck():
+    os.system("clear")
     playing_deck = COMPLETE_DECK
     shuffle(playing_deck)
     return playing_deck
@@ -32,70 +38,159 @@ def deal_card(hand, deck):
     # need to grab 2 card from deck
     hand.append(deck.pop())
 
-def display_hand(hand, is_dealer = False):
-    # if not is_dealer:
-    #     str = "You are holding: "
-    #     for card in range(0, len(hand)):
-    #         str += f"{hand[card][1]} of {player_hand[card][0]}"
-    # prompt(str)
+def display_hand(hand, is_dealer = False, conceal_card = False):
+    msg = ""
+    if is_dealer:
+        msg += "Dealer's hand: "
     if not is_dealer:
+        msg += "Your hand: "
+    if not conceal_card:
         legible_hand = [f"{card[1]} of {card[0]}"  for card in hand]
         joined_hand = join_or(legible_hand, last_joining_word='and')
-        prompt(f"Your hand: {joined_hand}.")
+        msg += f"{joined_hand}."
     else:
         legible_hand = [f"{card[1]} of {card[0]}"  for card in hand[:-1]]
         joined_hand = join_or(legible_hand, last_joining_word='and')
-        prompt(f"Dealer's hand: {joined_hand} and a mystery card.")
+        msg += f"{joined_hand} and a secret card."
+
+    prompt(msg)
+
+def total(cards):
+    # cards = [['H', '3'], ['S', 'Q'], ... ]
+    values = [card[1] for card in cards]
+
+    sum_val = 0
+    for value in values:
+        if value == "A":
+            sum_val += 11
+        elif value in ['J', 'Q', 'K']:
+            sum_val += 10
+        else:
+            sum_val += int(value)
+
+    # correct for Aces
+    aces = values.count("A")
+    while sum_val > 21 and aces:
+        sum_val -= 10
+        aces -= 1
+
+    return sum_val
 
 def is_busted(hand):
-    # check if hand is busted
+    if total(hand) > 21:
+        return True
+    else:
+        return False
+    
+def dealer_turn(hand, deck):
+    while total(hand) < 17:
+        deal_card(hand, deck)
+        prompt(f"Dealer is dealt a {hand[-1][1]} of {hand[-1][0]} from the deck...")
     pass
 
-# 1. Initialize deck
-
-playing_deck = initialize_deck()
-
-# 2. Deal cards to player and dealer
-
-player_hand = []
-computer_hand = []
-
-deal_card(player_hand, playing_deck)
-deal_card(player_hand, playing_deck)
-deal_card(computer_hand, playing_deck)
-deal_card(computer_hand, playing_deck)
-
-# display your hand
-
-display_hand(player_hand)
-
-# display one card from computer's hand
-
-display_hand(computer_hand, is_dealer = True)
-
-# 3. Player turn: hit or stay
 
 while True:
-    prompt("Do you hit (h) or stay (s)?")
+
+    prompt_with_separator("Welcome to Twenty-one.")
+
+    # 1. Initialize deck
+
+    playing_deck = initialize_deck()
+    winner = None
+
+    # 2. Deal cards to player and dealer
+
+    player_hand = []
+    computer_hand = []
+
+    deal_card(player_hand, playing_deck)
+    deal_card(player_hand, playing_deck)
+    deal_card(computer_hand, playing_deck)
+    deal_card(computer_hand, playing_deck)
+
+    # display your hand
+
+    display_hand(player_hand)
+
+    # display one card from computer's hand
+
+    display_hand(computer_hand, is_dealer = True, conceal_card=True)
+
     while True:
-        choice = input().strip()
-        if choice in ('h', 's'):
-            break
-        prompt("Please choose between hit (h) or stay (s).")
 
-    if choice == 's':
-        prompt("You decide to stay.")
+        # 3. Player turn: hit or stay
+        #    - repeat until bust or stay
+
+        while True:
+            prompt_with_separator("Do you hit (h) or stay (s)?")
+            while True:
+                choice = input().strip()
+                if choice in ('h', 's'):
+                    break
+                prompt_with_separator("Please choose between hit (h) or stay (s).")
+
+            if choice == 's':
+                prompt_with_separator("You decide to stay.")
+                break
+            if choice == 'h':
+                deal_card(player_hand, playing_deck)
+                display_hand(player_hand)
+                if is_busted(player_hand):
+                    winner = 'dealer'
+                    break
+
+    # 4. If player bust, dealer wins.
+
+        if winner:
+            prompt_with_separator("You busted!")
+            prompt_with_separator("Dealer wins!")
+            break
+
+        # 5. Dealer turn: hit or stay
+        #    - repeat until total >= 17
+
+        dealer_turn(computer_hand, playing_deck)
+
+        # reveal dealer hand
+
+        display_hand(computer_hand, is_dealer=True, conceal_card=False)
+
+        # 6. If dealer busts, player wins.
+
+        if is_busted(computer_hand):
+            prompt_with_separator("You win!")
+            break
+
+        # 7. Compare cards and declare winner.
+
+
+        if total(player_hand) > total(computer_hand):
+            prompt_with_separator("You win!")
+            break
+        if total(computer_hand) > total(player_hand):
+            prompt_with_separator("Dealer wins!")
+            break
+        else:
+            prompt_with_separator("It's a tie!")
+            break
+    
+    prompt_with_separator("Play again? (y or n)")
+
+    while True:
+        answer = input().lower()
+
+        if answer == "n" or answer == "y":
+            break
+        else:
+            prompt_with_separator("Please choose a valid option (y or n)")
+
+    if answer == "n":
         break
-    if choice == 'h':
-        deal_card(player_hand, playing_deck)
-        display_hand(player_hand)
-        if is_busted(player_hand):
-            break
+
+    prompt_with_separator("Thanks for playing Twenty-One!")
 
 
-#    - repeat until bust or stay
-# 4. If player bust, dealer wins.
-# 5. Dealer turn: hit or stay
-#    - repeat until total >= 17
-# 6. If dealer busts, player wins.
-# 7. Compare cards and declare winner.
+
+
+
+
