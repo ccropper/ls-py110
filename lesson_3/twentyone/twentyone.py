94fed4aa5949f5a18eaa13624bd82968b1d84fbc
Caiqian Cropper <caiqian.wu@gmail.com>
2024-04-27 21:45:26 -0500

    address some pylint suggestions

--- lesson_3/twentyone/twentyone.py
+++ lesson_3/twentyone/twentyone.py
@@ -2,45 +2,51 @@ import os
 import random
 from utils import prompt, join_or
 
-SUITS = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-VALUES = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', \
-'A')
+SUITS = ("Hearts", "Diamonds", "Spades", "Clubs")
+VALUES = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+
 
 def create_deck():
     return [[suit, value] for suit in SUITS for value in VALUES]
 
-def prompt_with_separator(msg, delimiter='~', width=60):
+
+def prompt_with_separator(msg, delimiter="-", width=60):
     print(f"{delimiter * width}")
     prompt(msg)
 
+
 def shuffle(cards):
     random.shuffle(cards)
+
 
 def initialize_deck():
     deck = create_deck()
     shuffle(deck)
     return deck
 
+
 def deal_card(hand, deck):
     # need to grab 2 card from deck
     hand.append(deck.pop())
 
-def display_hand(hand, is_dealer = False, conceal_card = False):
+
+def display_hand(hand, is_dealer=False, conceal_card=False):
     msg = ""
     if is_dealer:
         msg += "Dealer's hand: "
     if not is_dealer:
         msg += "Your hand: "
     if not conceal_card:
-        legible_hand = [f"{card[1]} of {card[0]}"  for card in hand]
-        joined_hand = join_or(legible_hand, last_joining_word='and')
+        legible_hand = [f"{card[1]} of {card[0]}" for card in hand]
+        joined_hand = join_or(legible_hand, last_joining_word="and")
         msg += f"{joined_hand}."
     else:
-        legible_hand = [f"{card[1]} of {card[0]}"  for card in hand[:-1]]
-        joined_hand = join_or(legible_hand, last_joining_word='and')
+        legible_hand = [f"{card[1]} of {card[0]}" for card in hand[:-1]]
+        joined_hand = join_or(legible_hand, last_joining_word="and")
         msg += f"{joined_hand} and a secret card."
 
     prompt(msg)
+
 
 def total(cards):
     # cards = [['H', '3'], ['S', 'Q'], ... ]
@@ -50,7 +56,7 @@ def total(cards):
     for value in values:
         if value == "A":
             sum_val += 11
-        elif value in ['J', 'Q', 'K']:
+        elif value in ["J", "Q", "K"]:
             sum_val += 10
         else:
             sum_val += int(value)
@@ -63,17 +69,20 @@ def total(cards):
 
     return sum_val
 
+
 def is_busted(hand):
     if total(hand) > 21:
         return True
-    else:
-        return False
-    
+    return False
+
+
 def dealer_turn(hand, deck):
     while total(hand) < 17:
         deal_card(hand, deck)
-        prompt(f"Dealer is dealt a {hand[-1][1]} of {hand[-1][0]} from the deck...")
-    pass
+        prompt(
+            f"Dealer hits! They are dealt a {hand[-1][1]} of {hand[-1][0]} from the deck..."
+        )
+    prompt_with_separator("Dealer stays.")
 
 
 def play_twentyone():
@@ -103,7 +112,7 @@ def play_twentyone():
 
         # display one card from computer's hand
 
-        display_hand(computer_hand, is_dealer = True, conceal_card=True)
+        display_hand(computer_hand, is_dealer=True, conceal_card=True)
 
         while True:
 
@@ -114,25 +123,28 @@ def play_twentyone():
                 prompt_with_separator("Do you hit (h) or stay (s)?")
                 while True:
                     choice = input().strip()
-                    if choice in ('h', 's'):
+                    if choice in ("h", "s"):
                         break
                     prompt_with_separator("Please choose between hit (h) or stay (s).")
 
-                if choice == 's':
-                    prompt_with_separator("You decide to stay.")
+                if choice == "s":
+                    prompt_with_separator(
+                        f"You decide to stay. Your hand total is {total(player_hand)}."
+                    )
                     break
-                if choice == 'h':
+                if choice == "h":
+                    prompt_with_separator("You decide to hit.")
                     deal_card(player_hand, playing_deck)
                     display_hand(player_hand)
                     if is_busted(player_hand):
-                        winner = 'dealer'
+                        winner = "dealer"
                         break
 
-        # 4. If player bust, dealer wins.
+            # 4. If player bust, dealer wins.
 
             if winner:
                 prompt_with_separator("You busted!")
-                prompt_with_separator("Dealer wins!")
+                prompt_with_separator("The dealer wins!")
                 break
 
             # 5. Dealer turn: hit or stay
@@ -143,15 +155,16 @@ def play_twentyone():
             # reveal dealer hand
 
             display_hand(computer_hand, is_dealer=True, conceal_card=False)
+            prompt(f"The dealer's hand total is {total(computer_hand)}.")
 
             # 6. If dealer busts, player wins.
 
             if is_busted(computer_hand):
+                prompt_with_separator("The dealer busted!")
                 prompt_with_separator("You win!")
                 break
 
             # 7. Compare cards and declare winner.
-
 
             if total(player_hand) > total(computer_hand):
                 prompt_with_separator("You win!")
@@ -159,26 +172,23 @@ def play_twentyone():
             if total(computer_hand) > total(player_hand):
                 prompt_with_separator("Dealer wins!")
                 break
-            else:
-                prompt_with_separator("It's a tie!")
-                break
-        
+            prompt_with_separator("It's a tie!")
+            break
+
         prompt_with_separator("Play again? (y or n)")
 
         while True:
             answer = input().lower()
 
-            if answer == "n" or answer == "y":
+            if answer in ("n", "y"):
                 break
-            else:
-                prompt_with_separator("Please choose a valid option (y or n)")
+
+            prompt_with_separator("Please choose a valid option (y or n)")
 
         if answer == "n":
             break
 
         prompt_with_separator("Thanks for playing Twenty-One!")
 
+
 play_twentyone()
-
-
-
